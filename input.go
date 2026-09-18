2577552b4d59ad967a06cd72ff4ad966f5df91b0
sharasha07 <sabasharashenidze07@gmail.com>
2026-09-18 14:54:03 +0400

    feat: add env/secret managing options

--- input.go
+++ input.go
@@ -2,4 +2,35 @@ package main
 
 type input struct {
 	moduleName string
+
+	env struct {
+		loader        envLoader
+		parser        envParser
+		secretManager envSecretManager
+	}
 }
+
+type envLoader int
+
+const (
+	noLoader envLoader = iota
+	dotenvx
+	godotenv
+)
+
+type envParser int
+
+const (
+	noParser envParser = iota
+	caarlosEnv
+)
+
+type envSecretManager int
+
+const (
+	noSecretManager envSecretManager = iota
+	dotenvxSecrets
+	awsSecretsManager
+	googleCloudSecretManager
+	azureKeyVault
+)
--- main.go
+++ main.go
@@ -24,6 +24,37 @@ func main() {
 					return nil
 				}),
 		),
+
+		huh.NewGroup(
+			huh.NewSelect[envLoader]().
+				Title("load environment variables:").
+				Options(
+					huh.NewOption("none", noLoader),
+					huh.NewOption(".env + github.com/joho/godotenv/tree/main (load .env variables into process environment)",
+						godotenv),
+					huh.NewOption(".env + dotenvx CLI (load .env without Go code dependencies)", dotenvx),
+				).
+				Value(&inp.env.loader),
+
+			huh.NewSelect[envParser]().
+				Title("parse environment variables:").
+				Options(
+					huh.NewOption("none", noParser),
+					huh.NewOption("github.com/caarlos0/env/v11 (map environment variables into typed Go struct)", caarlosEnv),
+				).
+				Value(&inp.env.parser),
+
+			huh.NewSelect[envSecretManager]().
+				Title("manage secrets:").
+				Options(
+					huh.NewOption("none", noSecretManager),
+					huh.NewOption("dotenvx (encrypt .env secrets while keeping .env workflow)", dotenvxSecrets),
+					huh.NewOption("AWS Secrets Manager", awsSecretsManager),
+					huh.NewOption("Google Cloud Secret Manager", googleCloudSecretManager),
+					huh.NewOption("Azure Key Vault", azureKeyVault),
+				).
+				Value(&inp.env.secretManager),
+		),
 	)
 
 	if err := form.Run(); err != nil {
